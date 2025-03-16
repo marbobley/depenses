267437b36d075e918bb044f48e3feb47969af3d8
wilfried <fournier.wilf@gmail.com>
2025-03-16 15:52:05 +0100

    still ihm

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('category')]
 final class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
+    #[Route('', name: 'app_category_index')]
     public function index(): Response
     {
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    #[Route('/new', name:'app_category_new')]
+    public function new(): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        return $this->render('category/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
