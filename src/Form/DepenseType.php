fb9cdd2351fae68f988a9df5a7df93a31c4dee2e
wilfried <fournier.wilf@gmail.com>
2025-03-22 18:37:51 +0100

    init controler and type depense

---
+++ src/Form/DepenseType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Depense;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DepenseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('created', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('amount')
+            ->add('createdBy', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Depense::class,
+        ]);
+    }
+}
