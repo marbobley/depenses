4f2e9f3a28d3a75d710deac67e9e112e5719c08e
will <fournier.wilf@gmail.com>
2025-03-27 16:55:28 +0000

    running php-cs-fixer

--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -11,36 +11,27 @@ class CategoryTest extends TestCase
     public function testsetgetName(): void
     {
         $category = new Category();
-        $category->setName("Hello");
-        
-        if($category->getName() === "Hello")
-        {
+        $category->setName('Hello');
+
+        if ('Hello' === $category->getName()) {
             $this->assertTrue(true);
-        }
-        else
-        {
+        } else {
             $this->assertTrue(false);
         }
     }
 
-    public function testsetgetCreatedBy(): void{
-
+    public function testsetgetCreatedBy(): void
+    {
         $user = new User();
-        $user-> setUsername("tata");
-
-
+        $user->setUsername('tata');
 
         $category = new Category();
         $category->setCreatedBy($user);
-        
-        if($category->getCreatedBy() === $user)
-        {
+
+        if ($category->getCreatedBy() === $user) {
             $this->assertTrue(true);
-        }
-        else
-        {
+        } else {
             $this->assertTrue(false);
         }
-
     }
 }
