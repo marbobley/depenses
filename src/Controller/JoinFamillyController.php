106b66182e42b7479f2acaaeb34df91c860862c8
will <fournier.wilf@gmail.com>
2025-03-27 19:21:05 +0000

    on going hashing try

--- src/Controller/JoinFamillyController.php
+++ src/Controller/JoinFamillyController.php
@@ -4,35 +4,27 @@ namespace App\Controller;
 
 use App\Entity\Family;
 use App\Repository\FamilyRepository;
-use App\Service\FamilyService;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Service\FamilyService;
 use Psr\Log\LoggerInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use Symfony\Component\HttpKernel\Log\Logger;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class JoinFamillyController extends AbstractController
 {
-    /*#[Route('/join/familly', name: 'app_joinfamilly_index')]
-    public function index(FamilyRepository $familyRepository): Response
-    {
-        $families = $familyRepository->findAll();
-
-        return $this->render('join_familly/index.html.twig', [
-            'controller_name' => 'JoinFamillyController',
-            'families' => $families
-        ]);
-    }*/
     #[Route('/join/familly', name: 'app_joinfamilly_index', methods: ['GET', 'POST'])]
-    public function index(Request $request, LoggerInterface $log, FamilyService $familyService, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, LoggerInterface $log, UserPasswordHasherInterface $userPasswordHasher,FamilyService $familyService, EntityManagerInterface $entityManager): Response
     {
-        $log->info('hell');
+        $log->info("hell");
 
         $defaultData = ['message' => 'Type your message here'];
         $form = $this->createFormBuilder($defaultData)
@@ -48,15 +40,15 @@ final class JoinFamillyController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // data is an array with "name", "email", and "message" keys
-            $data = $form->getData();
-
-            $family = $data['family'];
+            $data = $form->getData(); 
+            
+            $family = $data["family"];
 
             // configure different hashers via the factory
             $factory = new PasswordHasherFactory(['common' => ['algorithm' => 'bcrypt']]);
             $hasher = $factory->getPasswordHasher('common');
 
-            /*if($hasher->verify($family->getPassword2(), $data['plainPassword']) )
+            if($hasher->verify($family->getPassword2(), $data['plainPassword']) )
             {
                 // good password family
                 $log->info("good password");
@@ -66,23 +58,24 @@ final class JoinFamillyController extends AbstractController
             else {
                 //bad passwordfamily
                 $log->info($hasher->hash($data['plainPassword']));
-            }*/
+            }
+            
         }
-
+   
         return $this->render('join_familly/joinFamily.html.twig', [
             'form' => $form,
         ]);
     }
 
     #[Route('/{id}/join', name: 'app_joinfamily_join', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
-    public function join(?Family $family, FamilyService $familyService, EntityManagerInterface $entityManager): Response
+    public function join(?Family $family , FamilyService $familyService, EntityManagerInterface $entityManager): Response
     {
         if (null === $family) {
             // managing error
         }
 
         $user = $this->getUser();
-        $familyService->JoinFamily($family, $user, $entityManager);
+        $familyService->JoinFamily($family, $user , $entityManager);
 
         return $this->redirectToRoute('app_joinfamilly_index');
     }
--- src/Service/HasherService.php
+++ src/Service/HasherService.php
@@ -2,13 +2,8 @@
 
 namespace App\Service;
 
-use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
-
 class HasherService {
     public function hash(string $plainString) : string{
-
-        $factory = new PasswordHasherFactory(['common' => ['algorithm' => 'bcrypt']]);
-        $hasher = $factory->getPasswordHasher('common');
-        return $hasher->hash($plainString);
+        return hash('sha256', $plainString);
     }
 } 
