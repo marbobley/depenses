e9bbb3dff3408cd06b18ab5c3fb29c8edbcf6f20
will <fournier.wilf@gmail.com>
2025-03-28 16:36:05 +0000

    Password management family

--- src/Controller/Family/PasswordController.php
+++ src/Controller/Family/PasswordController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller\Family;
 
+use App\Entity\User;
+use App\Service\HasherService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PasswordController extends AbstractController
 {
-    #[Route('/family/password', name: 'app_family_password')]
-    public function index(): Response
+    #[Route('/family/password', name: 'app_family_password_index', methods: ['GET', 'POST'])]
+    public function index(Request $request, HasherService $hasher, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('family/password/index.html.twig', [
-            'controller_name' => 'PasswordController',
+        $defaultData = ['message' => 'Type your message here'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('plainPassword', PasswordType::class)
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            $plainPassword = $data['plainPassword'];
+            $hashPassword = $hasher->hash($plainPassword);
+            /** @var \App\Entity\User $user */
+            $user = $this->getUser();
+            $family = $user->getFamily();
+            $family->setPassword($hashPassword);
+            $entityManager->persist($family);
+            $entityManager->flush();
+        }
+
+        return $this->render('family/join/join.html.twig', [
+            'form' => $form,
         ]);
     }
 }
