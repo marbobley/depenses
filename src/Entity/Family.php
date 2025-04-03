c809b9e1179aefdcd9bee665567611cc3c06bc67
will <fournier.wilf@gmail.com>
2025-04-03 17:57:18 +0000

    stil playing

--- src/Entity/Family.php
+++ src/Entity/Family.php
@@ -102,11 +102,6 @@ class Family implements ICalculateAmount
         return $res;
     }
 
-    public function getPassword2(): string
-    {
-        return '$2y$13$.UPunezIFcxtdVV9FIC8.uoKqdUn0lEuDetcAgfzt858xj5uv4wG.';
-    }
-
     public function getPassword(): ?string
     {
         return $this->password;
