64268a5c77473112a7b529e13fe5e6c33dba1623
will <fournier.wilf@gmail.com>
2025-04-06 08:09:28 +0000

    mananing category from user in form depense

--- src/Form/DepenseType.php
+++ src/Form/DepenseType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Category;
 use App\Entity\Depense;
 use App\Entity\User;
+use App\Repository\CategoryRepository;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -12,9 +14,17 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Security;
 
 class DepenseType extends AbstractType
 {
+    public function __construct(private CategoryRepository $categoryRepository, private TokenStorageInterface $security)
+    {
+        
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -33,6 +43,7 @@ class DepenseType extends AbstractType
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
+                'choices' => $this->categoryRepository->findByUser($this->security->getToken()->getUser()),
             ])
         ;
     }
