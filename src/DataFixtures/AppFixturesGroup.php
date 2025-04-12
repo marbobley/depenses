48df889b9cc3c45c4e17743f14c2972b02a89885
will <fournier.wilf@gmail.com>
2025-04-12 17:10:35 +0000

    playing with groups

---
+++ src/DataFixtures/AppFixturesGroup.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\CategoryFactory;
+use App\Factory\DepenseFactory;
+use App\Factory\FamilyFactory;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixturesGroup extends Fixture  implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+    }
+
+    public static function getGroups(): array
+     {
+         return ['user'];
+     }
+}
