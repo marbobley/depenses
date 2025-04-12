d634ac3ef92ea97b0d904f1367d558ae0cfb42df
will <fournier.wilf@gmail.com>
2025-04-12 06:52:09 +0000

    Ongoing to clear database

--- src/Purger/CustomPurger.php
+++ src/Purger/CustomPurger.php
@@ -2,6 +2,9 @@
 
 namespace App\Purger;
 
+use App\Entity\Category;
+use App\Entity\Depense;
+use App\Entity\User;
 use Doctrine\Common\DataFixtures\Purger\ORMPurgerInterface;
 use Doctrine\Common\DataFixtures\Purger\PurgerInterface;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,7 +18,47 @@ class CustomPurger implements ORMPurgerInterface
     }
 
     public function purge(): void
-    {        
-        dd($this->em);
+    {       
+        $this->PurgeDepense();
+        $this->PurgeCategory(); 
+    }
+
+    private function PurgeCategory()
+    {
+        $repoCategory = $this->em->getRepository(Category::class);
+        $categories = $repoCategory->findAll();
+
+        foreach($categories as $category)
+        {
+            $this->em->remove($category);   
+            $this->em->flush();         
+        }
+    }
+
+    private function PurgeDepense()
+    {
+        $repoDepense = $this->em->getRepository(Depense::class);
+        $depenses = $repoDepense->findAll();
+
+        foreach($depenses as $depense)
+        {
+            $this->em->remove($depense);     
+            $this->em->flush();       
+        }
+    }
+
+    private function PurgeUser()
+    {
+        $repoUser = $this->em->getRepository(User::class);
+        $users = $repoUser->findAll();
+
+        foreach($users as $user)
+        {
+            $user->setFamily(null);
+            $this->em->persist($user);
+        }
+        $this->em->flush();
+
+        
     }
 }
