bb16e311829caf4359602629f7069ea045506720
will <fournier.wilf@gmail.com>
2025-04-12 06:26:22 +0000

    Fixe on purger

--- src/Purger/CustomPurger.php
+++ src/Purger/CustomPurger.php
@@ -2,12 +2,20 @@
 
 namespace App\Purger;
 
+use Doctrine\Common\DataFixtures\Purger\ORMPurgerInterface;
 use Doctrine\Common\DataFixtures\Purger\PurgerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
-class CustomPurger implements PurgerInterface
+class CustomPurger implements ORMPurgerInterface
 {
-    public function purge(): void
+    private EntityManagerInterface $em;
+    public function setEntityManager(EntityManagerInterface $em) : void
     {
-        // ...
+        $this->em = $em;
+    }
+
+    public function purge(): void
+    {        
+        dd($this->em);
     }
 }
