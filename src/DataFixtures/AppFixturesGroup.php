346cf867bf4d7376f9a70f99492e7d7c6a8f9277
will <fournier.wilf@gmail.com>
2025-04-13 07:37:06 +0000

    create 2 specif user admin et user

--- src/DataFixtures/AppFixturesGroup.php
+++ src/DataFixtures/AppFixturesGroup.php
@@ -2,19 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Factory\CategoryFactory;
+use App\Factory\DepenseFactory;
+use App\Factory\FamilyFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixturesGroup extends Fixture  implements FixtureGroupInterface
 {
+    public function __construct(private readonly UserPasswordHasherInterface $hasher)
+    {
+        
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $user = UserFactory::GetOneUser('admin', '12345', ['ROLE_ADMIN']);
-        $manager->persist($user);
-        $manager->flush();
-        UserFactory::createMany(10);
+        $password = $this->hasher->hashPassword(new User(), 'abcd1234!');
+
+        $admin = UserFactory::createOne(['userName' => 'admin' , 'roles' => ['ROLE_ADMIN'] , 'password' => $password]);
+        $user = UserFactory::createOne(['userName' => 'user' , 'roles' => ['ROLE_USER'] , 'password' => $password]);
+
+        UserFactory::createMany(8);
+        FamilyFactory::createMany(ConstantesFixtures::$numberOfFamily);
+        CategoryFactory::createMany(5);
+        DepenseFactory::createMany(100);
+        
     }
 
     public static function getGroups(): array
