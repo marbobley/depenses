20288bcd74d930af31dd843d7ed6cfdfc6f904e2
will <fournier.wilf@gmail.com>
2025-04-15 17:21:32 +0000

    Test some assertment

--- tests/Controller/MainControllerTest.php
+++ tests/Controller/MainControllerTest.php
@@ -4,10 +4,13 @@ namespace App\Tests\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 
 class MainControllerTest extends WebTestCase
-{
+{ 
+
     public function testNotConnected(): void
     {
         $client = static::createClient();
@@ -17,6 +20,40 @@ class MainControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'Gérez vos dépenses en quelques clics !');
     }
 
+    public function testNotConnectedClickOnCommencer(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', 'http://127.0.0.1:8000/');
+
+        $client->clickLink('Commencer');
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testNotConnectedClickOnConnexion(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', 'http://127.0.0.1:8000/');
+
+        $client->clickLink('Connexion');
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testNotConnectedClickOnInscription(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', 'http://127.0.0.1:8000/');
+
+        $client->clickLink('Inscription');
+        $this->assertResponseStatusCodeSame(200);
+    }
+    public function testNotConnectedClickOnDepenseLogo(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('POST', 'http://127.0.0.1:8000/');
+
+        $client->clickLink('Depenses');
+        $this->assertResponseStatusCodeSame(200);
+    }
 
     public function testNotConnectedAdminDepense(): void
     {
@@ -42,5 +79,39 @@ class MainControllerTest extends WebTestCase
         $client->request('GET', 'http://127.0.0.1:8000/');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Bonjour user!!!');
+    }
+
+    public function testVisitingAdminDepenseWhileLoggedInUserRole(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test user
+        $testUser = $userRepository->findOneBy(['username' => 'user']);
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        // test e.g. the profile page
+        $client->request('GET', 'http://127.0.0.1:8000/admin/depense');
+
+        $this->assertEquals(403, $client->getResponse()->getStatusCode());
+    }
+
+    public function testVisitingAdminCategoryWhileLoggedInUserRole(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        // retrieve the test user
+        $testUser = $userRepository->findOneBy(['username' => 'user']);
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        // test e.g. the profile page
+        $client->request('GET', 'http://127.0.0.1:8000/admin/category');
+
+        $this->assertEquals(403, $client->getResponse()->getStatusCode());
     }
 }
