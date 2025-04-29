a9784a0fce359c7532f353225247006ff6481056
will <fournier.wilf@gmail.com>
2025-04-29 11:47:19 +0000

    clean

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\Business\ServiceDepense;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(ServiceDepense $depenseService): Response
+    public function index(): Response
     {
         // I want depense grouped by cat for the month
         // Depense user then month then grou
