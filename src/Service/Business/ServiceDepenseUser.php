c43d22e0a0d679a9b8de0ced02a41f366262cd40
will <fournier.wilf@gmail.com>
2025-06-26 16:03:10 +0000

    Factoring 2 methods to sum depense for user

--- src/Service/Business/ServiceDepenseUser.php
+++ src/Service/Business/ServiceDepenseUser.php
@@ -12,42 +12,18 @@ class ServiceDepenseUser
 {
     public function __construct(
         private Security $security,
-        private ServiceDepense $serviceDepense)
-    {
-    }
+        private ServiceDepense $serviceDepense
+    ) {}
 
-    public function GetUserCurrentMonthDepenses(): float
+    public function GetConnectedUserDepenseMonth($currentMonth, $currentYear): float
     {
         $user = $this->security->getUser();
-
-        $currentMonth = date('n');
-        $currentYear = date('Y');
-
         return $this->serviceDepense->GetTotalMonth($user, $currentMonth, $currentYear);
     }
 
-    public function GetUserLastMonthDepenses(): float
+    public function GetConnectedUserDepenseYear($currentYear): float 
     {
         $user = $this->security->getUser();
-        $lastmonth = date('n') - 1;
-        $currentYear = date('Y');
-
-        return $this->serviceDepense->GetTotalMonth($user, $lastmonth, $currentYear);
-    }
-
-    public function GetUserCurrentYearDepenses(): float
-    {
-        $user = $this->security->getUser();
-        $currentYear = date('Y');
-
-        return $this->serviceDepense->GetTotalYear($user, $currentYear);
-    }
-
-    public function GetUserLastYearDepenses(): float
-    {
-        $user = $this->security->getUser();
-        $currentYear = date('Y') - 1;
-
         return $this->serviceDepense->GetTotalYear($user, $currentYear);
     }
 }
