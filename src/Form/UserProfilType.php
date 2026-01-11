edd3619a2e7c0ad018fb9b162a200682037eb162
Marbobley <fournier.wilf@gmail.com>
2026-01-11 19:19:21 +0100

    fix : issue on constraint lenght

--- src/Form/UserProfilType.php
+++ src/Form/UserProfilType.php
@@ -35,15 +35,12 @@ class UserProfilType extends AbstractType
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                    new NotBlank(message: 'Please enter a password'),
+                    new Length(
+                        min: 6,
+                        max: 4096,
+                        minMessage: 'Your password should be at least {{ limit }} characters'
+                    ),
                 ],
             ])
             ->add('family', EntityType::class, [
