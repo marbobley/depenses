bb06676529ba735d7fc556d3dad5d12ebc2b32d1
Marbobley <fournier.wilf@gmail.com>
2026-01-31 13:05:22 +0100

    ongoing TU

--- tests/Domain/ServiceImpl/DepenseDomainTest.php
+++ tests/Domain/ServiceImpl/DepenseDomainTest.php
@@ -9,6 +9,9 @@ use App\Domain\ServiceInterface\DepenseDomainInterface;
 use App\Domain\ServiceInterface\FamilyDomainInterface;
 use App\Domain\ServiceInterface\SommeDomainInterface;
 use App\Domain\ServiceInterface\UserDomainInterface;
+use App\Exception\FamilyNotFoundException;
+use App\Factory\DepenseModelFactory;
+use App\Factory\FamilyModelFactory;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Zenstruck\Foundry\Test\Factories;
@@ -17,12 +20,14 @@ class DepenseDomainTest extends TestCase
 {
     use Factories;
 
+    public const ID_USER = 1;
+    public const ID_FAMILY = 1;
     public const ID_CATEGORY = 1;
-    public const RESULT_EMPTY_DEPENSES = 0.0;
-    public const ID_DEPENSE = 1;
-    public const AMOUNT = 33.5;
-    public const ID_CATEGORY_OTHER = 33;
+    public const YEAR = '2024';
+    public const MONTHS = ['1', '2'];
 
+    private FamilyModelFactory $familyModelFactory;
+    private DepenseModelFactory $depenseModelFactory;
     private MockObject $sommeDomainMock;
     private MockObject $familyDomainMock;
     private MockObject $userDomainMock;
@@ -30,12 +35,55 @@ class DepenseDomainTest extends TestCase
 
     protected function setUp(): void
     {
+        $this->familyModelFactory = new FamilyModelFactory();
+        $this->depenseModelFactory = new DepenseModelFactory();
+
         $this->userDomainMock = $this->createMock(UserDomainInterface::class);
         $this->familyDomainMock = $this->createMock(FamilyDomainInterface::class);
         $this->sommeDomainMock = $this->createMock(SommeDomainInterface::class);
-        $this->depenseDomain = new DepenseDomain($this->sommeDomainMock,
+        $this->depenseDomain = new DepenseDomain(
+            $this->sommeDomainMock,
             $this->familyDomainMock,
-            $this->userDomainMock);
+            $this->userDomainMock
+        );
     }
 
+    public function testGetDepenseForCategoryForMonthWithFamily(): void
+    {
+        $family = $this->familyModelFactory->createOne();
+
+        $datetime = new \DateTimeImmutable('2024-01-01');
+
+        $depenses = $this->depenseModelFactory->createList(['created' => $datetime]);
+
+        $this->familyDomainMock->method('getFamily')->willReturn($family);
+        $this->familyDomainMock->method('getDepenses')->willReturn($depenses);
+        $this->sommeDomainMock->method('filteredOnCategoryAndSumDepense')->willReturn(22.0);
+
+        $depenses = $this->depenseDomain->getDepenseForCategoryForMonth(self::ID_USER, self::ID_CATEGORY, self::MONTHS, self::YEAR);
+
+
+        $this->assertNotNull($depenses, "les depenses n'ont pas été calculé");
+        $this->userDomainMock->expects(self::never())->method(self::anything());
+    }
+
+    public function testGetDepenseForCategoryForMonthWithoutFamily(): void
+    {
+        $family = $this->familyModelFactory->createOne();
+
+        $datetime = new \DateTimeImmutable('2024-01-01');
+
+        $depenses = $this->depenseModelFactory->createList(['created' => $datetime]);
+
+        $this->familyDomainMock->method('getFamily')->willThrowException(new FamilyNotFoundException());
+        $this->familyDomainMock->method('getDepenses')->willReturn($depenses);
+        $this->sommeDomainMock->method('filteredOnCategoryAndSumDepense')->willReturn(22.0);
+        $this->userDomainMock->method('getDepenses')->willReturn($depenses);
+
+        $depenses = $this->depenseDomain->getDepenseForCategoryForMonth(self::ID_USER, self::ID_CATEGORY, self::MONTHS, self::YEAR);
+
+
+        $this->assertNotNull($depenses, "les depenses n'ont pas été calculé");
+
+    }
 }
---
+++ tests/Domain/ServiceImpl/FamilyDomainTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Domain\ServiceImpl;
+
+use PHPUnit\Framework\TestCase;
+
+class FamilyDomainTest extends TestCase
+{
+}
